0c52ecf3295a3d712eb118e3cdce1e04da8dadb6
OKA Toshiyuki <oka-was@langedge.com>
2021-03-03 12:30:02 +0900

    取得するPDFを「【別紙１】各都道府県の検査陽性者の状況」に変更

--- tools/extract_today_pref_data.sh
+++ tools/extract_today_pref_data.sh
@@ -30,29 +30,35 @@ VAR_PRINT year
 VAR_PRINT reiwa
 VAR_PRINT today
 VAR_PRINT datestr
+VAR_PRINT yesterday
 VAR_PRINT pdfPath
 
 echo "$(date) -- check $today"
 
 OUTFILE=mhlw_pref/$yesterday.txt
+VAR_PRINT OUTFILE
 
 if [ -z "$force" ] && [ -f $OUTFILE ] && [ -s $OUTFILE ]; then
     echo "Already extracted: $OUTFILE"
     exit
 fi
 
-url=$(RUN_CMD "curl $pageUrl 2>/dev/null | grep -m 1 '厚生労働省の対応について.*令和${reiwa}年$today' | cut -d'\"' -f2")
+url=$(RUN_CMD -f "curl $pageUrl 2>/dev/null | grep -m 1 '厚生労働省の対応について.*令和${reiwa}年$today' | cut -d'\"' -f2")
 VAR_PRINT -f url
 if [ "$url" ]; then
-    pdfUrl=$(RUN_CMD "curl $url 2>/dev/null | grep -m 1 '都道府県別のPCR検査陽性者数.*${year}年$today' | cut -d'\"' -f2")
+    #pdfUrl=$(RUN_CMD -f "curl $url 2>/dev/null | \
+    #    grep -m 1 '都道府県別のPCR検査陽性者数.*${year}年$today' | cut -d'\"' -f2")
+    pdfUrl=$(RUN_CMD -f "curl $url 2>/dev/null | \
+        grep -m 1 'content/[0-9/]*\.pdf.*各都道府県の検査陽性者の状況' | cut -d'\"' -f2")
     VAR_PRINT -f pdfUrl
     if [ "$pdfUrl" ]; then
         [[ "$pdfUrl" == /* ]] && pdfUrl="https://www.mhlw.go.jp$pdfUrl"
-        RUN_CMD -m "curl $pdfUrl -o $pdfPath"
-        RUN_CMD -m "/usr/local/bin/docker-compose run --rm camelot | tail -n +2 > $OUTFILE"
+        RUN_CMD -fm "curl $pdfUrl -o $pdfPath"
+        RUN_CMD -fm "/usr/local/bin/docker-compose run --rm camelot | \
+            sed -ne '2,/^合計/ s/ *//gp' | sed -re 's/\r//' > $OUTFILE"
         if [ -f $OUTFILE ] && [ -s $OUTFILE ]; then
             echo "Data extracted: $OUTFILE"
-            $BINDIR/commit-push.sh
+            RUN_CMD -m "$BINDIR/commit-push.sh"
             echo "$(date) -- extracted $today"
             exit
         fi
