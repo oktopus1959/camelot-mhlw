e3db70035734d467ebaa177eb0d4a6f61c7a1027
OKA Toshiyuki <oka-was@langedge.com>
2021-06-22 18:03:08 +0900

    update on 2021/06/22

--- tools/extract_today_tokyo_iryo.sh
+++ tools/extract_today_tokyo_iryo.sh
@@ -15,12 +15,14 @@ fi
 
 todayMMDD=$(eval "date $dateOpt '+%m%d'")
 today=$(eval "date $dateOpt '+%Y%m%d'")
-pdfUrl="https://www.fukushihoken.metro.tokyo.lg.jp/iryo/kansen/kunishihyou.files/${todayMMDD}.pdf"
+pdfUrl="https://www.fukushihoken.metro.tokyo.lg.jp/iryo/kansen/kunishihyou.files/kuni${todayMMDD}.pdf"
 pdfPath=work_pdf/tokyo_iryo_$today.pdf
+pdfPathRemote=${pdfPath/work/tokyo}
 OUTFILE=work_tokyo/tokyo_iryo_$today.txt
 
 VAR_PRINT today
 VAR_PRINT pdfPath
+VAR_PRINT pdfPathRemote
 VAR_PRINT OUTFILE
 
 echo "$(date) -- check $today"
@@ -34,8 +36,10 @@ RUN_CMD -fm "mkdir -p work_pdf work_tokyo"
 RUN_CMD -fm -y "curl $pdfUrl -o $pdfPath 2>/dev/null"
 fileSize=$(wc -c < $pdfPath)
 if [ -f $pdfPath ] && [ $fileSize -gt 10000 ] && ! grep '404 Not Found' $pdfPath ; then
-    RUN_CMD -fm "sudo docker-compose \
-        run --rm camelot python /root/mhlw_pref_pdf_to_text.py /$pdfPath > $OUTFILE"
+    # RUN_CMD -fm "sudo docker-compose run --rm camelot python /root/mhlw_pref_pdf_to_text.py /$pdfPath > $OUTFILE"
+    RUN_CMD -fm "cat $pdfPath | ssh 192.168.1.162 'cat > $pdfPathRemote; pdf2txt.py $pdfPathRemote' | \
+        sed -rn 's/^（([0-9,]*)人\/1,207床）.*$/\1/p' | sed -r 's/,//g' | head -1 > $OUTFILE"
+
     if [ -f $OUTFILE ] && [ -s $OUTFILE ]; then
         echo "Data extracted: $OUTFILE"
         #RUN_CMD -m "$BINDIR/commit-push.sh"
