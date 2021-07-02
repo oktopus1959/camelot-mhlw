b8bbc45273185c46188dcfce7e8d3757ea4a8ae3
OKA Toshiyuki <oka-was@langedge.com>
2021-07-02 18:33:09 +0900

    update on 2021/07/02

--- tools/extract_today_tokyo_iryo.sh
+++ tools/extract_today_tokyo_iryo.sh
@@ -15,7 +15,8 @@ fi
 
 todayMMDD=$(eval "date $dateOpt '+%m%d'")
 today=$(eval "date $dateOpt '+%Y%m%d'")
-pdfUrl="https://www.fukushihoken.metro.tokyo.lg.jp/iryo/kansen/kunishihyou.files/kuni${todayMMDD}.pdf"
+pdfUrl1="https://www.fukushihoken.metro.tokyo.lg.jp/iryo/kansen/kunishihyou.files/${todayMMDD}.pdf"
+pdfUrl2="https://www.fukushihoken.metro.tokyo.lg.jp/iryo/kansen/kunishihyou.files/kuni${todayMMDD}.pdf"
 pdfPath=work_pdf/tokyo_iryo_$today.pdf
 pdfPathRemote=${pdfPath/work/tokyo}
 OUTFILE=work_tokyo/tokyo_iryo_$today.txt
@@ -33,8 +34,12 @@ if [ -z "$force" ] && [ -f $OUTFILE ] && [ -s $OUTFILE ]; then
 fi
 
 RUN_CMD -fm "mkdir -p work_pdf work_tokyo"
-RUN_CMD -fm -y "curl $pdfUrl -o $pdfPath 2>/dev/null"
+RUN_CMD -fm -y "curl $pdfUrl1 -o $pdfPath 2>/dev/null"
 fileSize=$(wc -c < $pdfPath)
+if [ ! -f $pdfPath ] || [ $fileSize -lt 10000 ] || grep '404 Not Found' $pdfPath ; then
+    RUN_CMD -fm -y "curl $pdfUrl2 -o $pdfPath 2>/dev/null"
+    fileSize=$(wc -c < $pdfPath)
+fi
 if [ -f $pdfPath ] && [ $fileSize -gt 10000 ] && ! grep '404 Not Found' $pdfPath ; then
     # RUN_CMD -fm "sudo docker-compose run --rm camelot python /root/mhlw_pref_pdf_to_text.py /$pdfPath > $OUTFILE"
     RUN_CMD -fm "cat $pdfPath | ssh 192.168.1.162 'cat > $pdfPathRemote; pdf2txt.py $pdfPathRemote' | \
