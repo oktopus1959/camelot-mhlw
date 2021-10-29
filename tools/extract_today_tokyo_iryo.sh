6a89699a69e4da9028cd143d096e2e423cf68a12
OKA Toshiyuki <oka-was@langedge.com>
2021-10-29 18:03:06 +0900

    update on 2021/10/29

--- tools/extract_today_tokyo_iryo.sh
+++ tools/extract_today_tokyo_iryo.sh
@@ -15,15 +15,24 @@ fi
 
 todayMMDD=$(eval "date $dateOpt '+%m%d'")
 today=$(eval "date $dateOpt '+%Y%m%d'")
-pdfUrl1="https://www.fukushihoken.metro.tokyo.lg.jp/iryo/kansen/corona_portal/info/kunishihyou.files/kuni${todayMMDD}.pdf"
+
+baseUrl="https://www.fukushihoken.metro.tokyo.lg.jp/iryo/kansen/corona_portal/info"
+pdfFile=$(curl $baseUrl/kunishihyou.html 2>/dev/null | egrep "filelink.*${todayMMDD}.*国のステージ判断のための指標" | sed -rn 's/^.* href="([^"]+)" .*$/\1/p')
+
+[ "$pdfFile" ] && pdfUrl0=$baseUrl/$pdfFile
+
+pdfUrl11="https://www.fukushihoken.metro.tokyo.lg.jp/iryo/kansen/corona_portal/info/kunishihyou.files/kuni${todayMMDD}.pdf"
+pdfUrl12="https://www.fukushihoken.metro.tokyo.lg.jp/iryo/kansen/corona_portal/info/kunishihyou.files/${todayMMDD}kuni.pdf"
 pdfUrl2="https://www.fukushihoken.metro.tokyo.lg.jp/iryo/kansen/corona_portal/info/kunishihyou.files/${todayMMDD}.pdf"
-pdfUrl3="https://www.fukushihoken.metro.tokyo.lg.jp/iryo/kansen/kunishihyou.files/kuni${todayMMDD}.pdf"
+pdfUrl31="https://www.fukushihoken.metro.tokyo.lg.jp/iryo/kansen/kunishihyou.files/kuni${todayMMDD}.pdf"
+pdfUrl32="https://www.fukushihoken.metro.tokyo.lg.jp/iryo/kansen/kunishihyou.files/${todayMMDD}kuni.pdf"
 pdfUrl4="https://www.fukushihoken.metro.tokyo.lg.jp/iryo/kansen/kunishihyou.files/${todayMMDD}.pdf"
 pdfPath=work_pdf/tokyo_iryo_$today.pdf
 pdfPathRemote=${pdfPath/work/tokyo}
 OUTFILE=work_tokyo/tokyo_iryo_$today.txt
 
 VAR_PRINT today
+VAR_PRINT pdfUrl0
 VAR_PRINT pdfPath
 VAR_PRINT pdfPathRemote
 VAR_PRINT OUTFILE
@@ -36,14 +45,26 @@ if [ -z "$force" ] && [ -f $OUTFILE ] && [ -s $OUTFILE ]; then
 fi
 
 RUN_CMD -fm "mkdir -p work_pdf work_tokyo"
-RUN_CMD -fm -y "curl $pdfUrl1 -o $pdfPath 2>/dev/null"
+RUN_CMD -fm -y "curl $pdfUrl0 -o $pdfPath 2>/dev/null"
 fileSize=$(wc -c < $pdfPath)
+if [ ! -f $pdfPath ] || [ $fileSize -lt 10000 ] || grep '404 Not Found' $pdfPath ; then
+    RUN_CMD -fm -y "curl $pdfUrl11 -o $pdfPath 2>/dev/null"
+    fileSize=$(wc -c < $pdfPath)
+fi
+if [ ! -f $pdfPath ] || [ $fileSize -lt 10000 ] || grep '404 Not Found' $pdfPath ; then
+    RUN_CMD -fm -y "curl $pdfUrl12 -o $pdfPath 2>/dev/null"
+    fileSize=$(wc -c < $pdfPath)
+fi
 if [ ! -f $pdfPath ] || [ $fileSize -lt 10000 ] || grep '404 Not Found' $pdfPath ; then
     RUN_CMD -fm -y "curl $pdfUrl2 -o $pdfPath 2>/dev/null"
     fileSize=$(wc -c < $pdfPath)
 fi
 if [ ! -f $pdfPath ] || [ $fileSize -lt 10000 ] || grep '404 Not Found' $pdfPath ; then
-    RUN_CMD -fm -y "curl $pdfUrl3 -o $pdfPath 2>/dev/null"
+    RUN_CMD -fm -y "curl $pdfUrl31 -o $pdfPath 2>/dev/null"
+    fileSize=$(wc -c < $pdfPath)
+fi
+if [ ! -f $pdfPath ] || [ $fileSize -lt 10000 ] || grep '404 Not Found' $pdfPath ; then
+    RUN_CMD -fm -y "curl $pdfUrl32 -o $pdfPath 2>/dev/null"
     fileSize=$(wc -c < $pdfPath)
 fi
 if [ ! -f $pdfPath ] || [ $fileSize -lt 10000 ] || grep '404 Not Found' $pdfPath ; then
